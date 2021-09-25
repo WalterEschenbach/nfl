70a530da613b9bcd950a6f392cde1ce8dbadc516
Walter <eschenbach.walter@gmail.com>
2021-09-25 17:58:29 -0500

    api key header to news api request

--- src/utils/getNews.js
+++ src/utils/getNews.js
@@ -4,13 +4,19 @@ const getNews = async (team1slug, team2slug) => {
 
     const team1 = await axios({
         method: "GET",
-        url: `https://newsapi.org/v2/everything?qInTitle=${team1slug}&sortBy=publishedAt&language=en&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
+        url: `https://newsapi.org/v2/everything?qInTitle=${team1slug}&sortBy=publishedAt&language=en`,
+        headers: {
+            "x-api-key": process.env.REACT_APP_NEWS_API_KEY
+        }
     }).catch(error => {
         console.log(error)
     })
     const team2 = await axios({
         method: "GET",
-        url: `https://newsapi.org/v2/everything?qInTitle=${team2slug}&sortBy=publishedAt&language=en&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
+        url: `https://newsapi.org/v2/everything?qInTitle=${team2slug}&sortBy=publishedAt&language=en`,
+        headers: {
+            "x-api-key": process.env.REACT_APP_NEWS_API_KEY
+        }
     }).catch(error => {
         console.log(error)
     })
