c7f9f56003b088c5af9077985af40d365c4ba6fa
Walter <eschenbach.walter@gmail.com>
2021-09-25 17:43:55 -0500

    error handling from newsapi call

--- src/utils/getNews.js
+++ src/utils/getNews.js
@@ -5,10 +5,14 @@ const getNews = async (team1slug, team2slug) => {
     const team1 = await axios({
         method: "GET",
         url: `https://newsapi.org/v2/everything?qInTitle=${team1slug}&sortBy=publishedAt&language=en&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
+    }).catch(error => {
+        console.log(error)
     })
     const team2 = await axios({
         method: "GET",
         url: `https://newsapi.org/v2/everything?qInTitle=${team2slug}&sortBy=publishedAt&language=en&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
+    }).catch(error => {
+        console.log(error)
     })
 
     const teams = [team1, team2]
