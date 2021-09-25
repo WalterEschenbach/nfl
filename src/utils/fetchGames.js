2b8f71dec4dc7f51e8aaf4f30cd946ad59c8aaec
Walter <eschenbach.walter@gmail.com>
2021-09-25 17:12:55 -0500

    minor update to add environment variables

--- src/utils/fetchGames.js
+++ src/utils/fetchGames.js
@@ -7,7 +7,7 @@ const fetchGames = () => {
         method: "GET",
         url: "https://odds.p.rapidapi.com/v1/odds?sport=americanfootball_nfl&region=us&mkt=totals",
         headers: {
-            'x-rapidapi-key': keys.NFL_API_KEY,
+            'x-rapidapi-key': process.env.NFL_API_KEY || keys.NFL_API_KEY,
             'x-rapidapi-host': 'odds.p.rapidapi.com'
         }
 
